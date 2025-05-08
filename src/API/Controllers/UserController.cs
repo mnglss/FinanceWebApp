52710598a9d556cb4bca5cb921ce4b4757c6633f
Alessio MENGHINI <alessiomenghini.dev@outlook.it>
2025-05-08 18:05:27 +0200

    Fix Db

--- src/API/Controllers/UserController.cs
+++ src/API/Controllers/UserController.cs
@@ -1,6 +1,16 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
 namespace FinanceWebApp.API.Controllers
 {
     public class UserController : BaseApiController
     {
+        [Authorize]
+        [HttpGet()]
+        public string[] GetUser()
+        {
+            return ["User1", "Uer2", "User3"];
+        }
     }
 }
---
+++ src/Infrastructure/Migrations/20250508153629_SyncDb.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                schema: "FinanceWebApp",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Date",
+                schema: "FinanceWebApp",
+                table: "Movements",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(2025, 5, 8),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldDefaultValue: new DateOnly(2025, 5, 7));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordSalt",
+                schema: "FinanceWebApp",
+                table: "Users",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Date",
+                schema: "FinanceWebApp",
+                table: "Movements",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(2025, 5, 7),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldDefaultValue: new DateOnly(2025, 5, 8));
+        }
+    }
+}
