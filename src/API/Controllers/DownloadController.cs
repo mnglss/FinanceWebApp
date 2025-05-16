a9a62d761c2e5ef9f65bf00bb09deb8633a862ef
Alessio MENGHINI <alessiomenghini.dev@outlook.it>
2025-05-16 17:19:30 +0200

    Add Movement Export Pdf

--- src/API/Controllers/DownloadController.cs
+++ src/API/Controllers/DownloadController.cs
@@ -22,5 +22,27 @@ namespace API.Controllers
             var file = ExcelService.CreateFile(movementlist);
             return File(file, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ListaMovimenti.xlsx");
         }
+
+        [HttpPost("Pdf")]
+        public async Task<FileResult> Pdf([FromBody, Required] MovementByUserIdRequest request)
+        {
+            List<Movement> movementlist = [];
+            var result = await movementService.GetByUserIdAsync(request);
+            if (result!.IsFailure)
+                movementlist.Add(new Movement { Year = DateTime.Now.Year, Month = DateTime.Now.Month, Date = DateOnly.FromDateTime(DateTime.Now), Amount = 0, Description = "Nessun Risultato" });
+            else
+                movementlist = result.Value;
+            var pdf = PdfService.MovementPdf(movementlist);
+
+            MemoryStream stream = new MemoryStream();
+            pdf.Save(stream);
+
+            Response.ContentType = "application/pdf";
+            Response.Headers.Add("content-length", stream.Length.ToString());
+            byte[] file = stream.ToArray();
+            stream.Close();
+
+            return File(file, "application/pdf", "Lista_Movimenti.pdf");
+        }
     }
 }
---
+++ src/Application/Services/PdfService.cs
@@ -0,0 +1,84 @@
+﻿using Domain.Entities;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Fields;
+using MigraDoc.DocumentObjectModel.Tables;
+using MigraDoc.Rendering;
+using PdfSharp.Pdf;
+
+namespace Application.Services
+{
+    public static class PdfService
+    {
+        public static PdfDocument MovementPdf(List<Movement> movementList)
+        {
+            var document = new Document();
+
+            BuildDocument(document, movementList);
+
+            var pdfRederer = new PdfDocumentRenderer();        
+            pdfRederer.Document = document;
+
+            pdfRederer.RenderDocument();
+
+            return pdfRederer.PdfDocument;
+        }
+
+        private static void BuildDocument(Document document, List<Movement> movementList)
+        {
+            Section section = new Section();
+            section.PageSetup.StartingNumber = 1;
+
+            
+
+            var paragraph = section.AddParagraph();
+            paragraph.AddText("Lista Movimenti");
+            paragraph.AddLineBreak();
+            paragraph.Format.SpaceAfter = 20;
+
+            paragraph = section.AddParagraph();
+            paragraph.AddText($"Aggiornata al: ");
+            paragraph.Add(new DateField { Format = "dd/MM/yyy" });
+            paragraph.AddLineBreak();
+            paragraph.Format.SpaceAfter = 20;
+            
+            document.Sections.Add(section);
+            var table = document.LastSection.AddTable();
+            table.Borders.Width = 0.5;
+            table.AddColumn("2cm");
+            table.AddColumn("1,5cm");
+            table.AddColumn("3cm");
+            table.AddColumn("4cm");
+            table.AddColumn("4cm");
+            table.AddColumn("3cm");
+
+            Row row = table.AddRow();
+            row.HeadingFormat = true;
+            row.Format.Font.Bold = true;
+            row.Cells[0].AddParagraph("Anno");
+            row.Cells[1].AddParagraph("Mese");
+            row.Cells[2].AddParagraph("Data");
+            row.Cells[3].AddParagraph("Categoria");
+            row.Cells[4].AddParagraph("Descrizione");
+            row.Cells[5].AddParagraph("Importo");
+
+            var totale = 0.0;
+            foreach (var movement in movementList)
+            {
+                row = table.AddRow();
+                row.Cells[0].AddParagraph($"{movement.Year}");
+                row.Cells[1].AddParagraph($"{movement.Month}");
+                row.Cells[2].AddParagraph(movement.Date.ToShortDateString());
+                row.Cells[3].AddParagraph(movement.Category);
+                row.Cells[4].AddParagraph(movement.Description);
+                row.Cells[5].AddParagraph($"{movement.Amount}");
+                totale += movement.Amount;
+            }
+
+            row = table.AddRow();
+            row[4].AddParagraph("Rimanenza");
+            row[5].AddParagraph($"{Math.Round(totale,2)}");
+
+
+        }
+    }
+}
